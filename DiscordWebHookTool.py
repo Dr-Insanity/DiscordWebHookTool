9cd744e0656aefb48ce5d1a67a34bc344f3df6af
Dr-insanity <cichonquincy@gmail.com>
2021-09-10 02:56:01 +0200

    Fixed a typo in README.MD. Significantly improved and stabilised the code

--- DiscordWebHookTool.py
+++ DiscordWebHookTool.py
@@ -16,36 +16,109 @@ def send():
     config.read('config.ini')
     W_URL = config.get('discord_webhook_tool', 'URL')
 
+    try:
+        hook = Webhook(W_URL)
+        arg = input(f"[Discord Webhook Tool] webhook> ")
+
+        if arg != "embed": # just send text, if input was not embed
+            hook.send(arg)
+
+        elif arg == "embed": # send embeds this way (or, if you want to do it your own way, you may write your own code below)
+            embed = Embed(
+                description="The description of the embed",
+                color=0x2f3136, # pick a color (this is a hex-based color value in this example)
+                timestamp='now' # sets the timestamp to current time
+                )
+
+            embed.set_author(name='Dr-Insanity') # an author (feel free to change)
+            embed.add_field(name="Google", value='[click to go to google](https://google.com/)') # this is an example of a link (with blue text on the embed)
+            embed.set_footer(text='the text on the footer')
+
+            hook.send(embed=embed)
+
+        else:
+            if arg == "x": # exit the application
+                exit()
+    except ValueError:
+        os.system("clear")
+        print(
+"""
+╔═─═─═─═─═─═─═─═─═─═─═─═─═─═╗                        
+║ ❌ webhook URL invalid ❌ ║
+╚═─═─═─═─═─═─═─═─═─═─═─═─═─═╝
+""")
+        # Ask for the webhook URL to use
+        webhook_URL = input("specify the webhook URL> ")
+        config.read('config.ini')
+        config.set('discord_webhook_tool', 'URL', webhook_URL)
+            
+        with open('config.ini', 'w') as f:
+            config.write(f)
+        
+    send()
+
+def ChangeWebHookURL():
+    try:
+        open('config.ini', 'r')
+        # Ask for the webhook URL to use
+        webhook_URL = input("specify the webhook URL> ")
+        config.read('config.ini')
+        config.set('discord_webhook_tool', 'URL', webhook_URL)
+        send()
+
+    except FileNotFoundError: # We assume URL was never been given to use.
+
+        # Ask for the webhook URL to use
+        webhook_URL = input("specify the webhook URL> ")
+        config.read('config.ini')
+        config.add_section('discord_webhook_tool')
+        config.set('discord_webhook_tool', 'URL', webhook_URL)
+        
+        with open('config.ini', 'w') as f:
+            config.write(f)
+        send()
+
+def Q_ChangeHook_InvalidReply():
+    os.system("clear")
     print(
 """
-╔═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═╗                        
-║ ✅ webhook URL found in configuration ✅ ║
-╚═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═╝
+╔═─═─═─═─═─═─═─═─═─═─═─═─═─═╗                        
+║ ❌    Invalid reply    ❌ ║
+╚═─═─═─═─═─═─═─═─═─═─═─═─═─═╝
+Your choices are:
+- use
+- change""")
+    time.sleep(3)
+    ConfigCheck()
+
+def ConfigCheck():
+    os.system("clear")
+    try:
+        open('config.ini', 'r')
+        
+        print(
+"""
+╔═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═╗
+║      🔗        Discord Webhook Tool       🔗        ║
+║ Hey there!   I found a webhook in the configuration ║
+╚═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═╝
 """)
-    hook = Webhook(W_URL)
-    arg = input(f"[Discord Webhook Tool] webhook> ")
+        YesOrNo = input("Do you wish to (use) this or (change)?\nType 'use' or 'change' to make a choice> ")
 
-    if arg != "embed": # just send text, if input was not embed
-        hook.send(arg)
+        proper_replies = ['use', 'change']
 
-    elif arg == "embed": # send embeds this way (or, if you want to do it your own way, you may write your own code below)
-        embed = Embed(
-            description="The description of the embed",
-            color=0x2f3136, # pick a color (this is a hex-based color value in this example)
-            timestamp='now' # sets the timestamp to current time
-            )
+        if YesOrNo.lower() in proper_replies:
+            if YesOrNo.lower() == 'use':
+                send()
+            elif YesOrNo.lower() == 'change':
+                os.system('clear')
+                ChangeWebHookURL()
+        else:
+            Q_ChangeHook_InvalidReply()
 
-        embed.set_author(name='Dr-Insanity') # an author (feel free to change)
-        embed.add_field(name="Google", value='[click to go to google](https://google.com/)') # this is an example of a link (with blue text on the embed)
-        embed.set_footer(text='the text on the footer')
+    except FileNotFoundError: # We assume URL was never been given to use.
 
-        hook.send(embed=embed)
-
-    else:
-        if arg == "x": # exit the application
-            exit()
-
-    send()
+        InitWebhook() # continue the application
 
 def InitWebhook():
     # start with clean terminal
@@ -56,15 +129,17 @@ def InitWebhook():
     except FileNotFoundError: # We assume URL was never been given to use.
 
         # Ask for the webhook URL to use
-        webhook_URL = input("What is the webhook's URL? (We'll just assume you gave a functional and existing webhook URL)> ")
+        webhook_URL = input("specify the webhook URL> ")
         config.read('config.ini')
         config.add_section('discord_webhook_tool')
         config.set('discord_webhook_tool', 'URL', webhook_URL)
         
         with open('config.ini', 'w') as f:
             config.write(f)
+
     send()
 def main():
+    os.system("clear")
     print(
         """
         ╔═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═─═╗
@@ -79,6 +154,7 @@ def main():
 
     time.sleep(5) # give people the time to read front page of this CLI-based utility
     os.system("clear") # Clear the current terminal / CMD / Powershell Window
-    InitWebhook() # continue the application
+    
+    ConfigCheck()
 
 main()
